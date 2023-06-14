40ed9a2944afd0ec0a4113971bc428c23a0f1676
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2023-06-14 23:25:48 +0200

    Use promises instead of callbacks

--- src/helpers/fetchPlaylist.js
+++ src/helpers/fetchPlaylist.js
@@ -4,7 +4,7 @@
  * Created Date: 22.04.2022 19:47:40
  * Author: 3urobeat
  * 
- * Last Modified: 24.04.2022 17:31:01
+ * Last Modified: 14.06.2023 23:24:05
  * Modified By: 3urobeat
  * 
  * Copyright (c) 2022 3urobeat <https://github.com/HerrEurobeat>
@@ -25,82 +25,85 @@ const config = require("../../config.json");
  * Internal function to fetch playlist content from Spotify
  * @param {String} playlistID The playlist to fetch from Spotify
  * @param {String} oAuthToken The Spotify oAuthToken to authorize the request with
- * @param {function} [callback] Called with `err` (String) on failure or `itemsObj` (Object) on success
+ * @returns {Promise.<array.<string>>} Returns a promise that is resolved with an array, containing all song objects, or rejected with an error message.
  */
-module.exports.fetchPlaylist = (playlistID, oAuthToken, callback) => {
+module.exports.fetchPlaylist = (playlistID, oAuthToken) => {
+    return new Promise((resolve, reject) => {
 
-    //arr for collecting all items
-    const itemsObj = []
+        //arr for collecting all items
+        const items = []
 
-    //put fetching into function to be able to call it recursively for each part
-    let fetchPart = (offset, part) => {
+        //put fetching into function to be able to call it recursively for each part
+        let fetchPart = (offset, part) => {
 
-        //log debug and info msg
-        logger("debug", `fetchPart(): Called with offset ${offset} & part ${part}`)
-        logger("info", `Fetching playlist part ${part}...`, false, true, logger.animation("loading"));
+            //log debug and info msg
+            logger("debug", `fetchPart(): Called with offset ${offset} & part ${part}`)
+            logger("info", `Fetching playlist part ${part}...`, false, true, logger.animation("loading"));
 
-        //Construct get request options obj - https://developer.spotify.com/console/get-playlist-tracks/
-        let options = {
-            hostname: "api.spotify.com",
-            path: `/v1/playlists/${playlistID}/tracks?offset=${offset}&fields=items(track(artists(name)%2Cname))%2Ctotal`, //filter: track name, artists names, total songs in playlist
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${oAuthToken}`
+            //Construct get request options obj - https://developer.spotify.com/console/get-playlist-tracks/
+            let options = {
+                hostname: "api.spotify.com",
+                path: `/v1/playlists/${playlistID}/tracks?offset=${offset}&fields=items(track(artists(name)%2Cname))%2Ctotal`, //filter: track name, artists names, total songs in playlist
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${oAuthToken}`
+                }
             }
+
+            //make get request
+            let request = https.request(options, (res) => {
+                let data = "";
+                let body;
+
+                //listen for recieved data
+                res.on("data", (chunk) => {
+                    data += chunk.toString();
+                });
+
+                //listen for completed request event
+                res.on("end", () => {
+
+                    //try to convert recieved string into object
+                    try {
+                        body = JSON.parse(data);
+                    } catch (err) {
+                        reject(`Failed to parse response: ${err}`);
+                    }
+
+                    //Check if spotify responded with an error, otherwise resolve with data
+                    if (body["error"]) {
+                        reject(`Spotify responded with error ${body.error.status}: ${body.error.message}`);
+                    } else {
+
+                        //push all objects of new array to existing array
+                        body.items.every(e => items.push(e))
+
+                        //Check if we have to fetch more parts and call function again
+                        if (body.total > (part + 1) * 100) {
+                            logger("info", `Finished fetching playlist part ${part}, waiting ${config.fetchDelay}ms before continuing with next part...`, false, true, logger.animation("loading"));
+
+                            //call this function again after fetchDelay ms with increased values
+                            setTimeout(() => fetchPart(offset + 100, part + 1), config.fetchDelay);
+
+                        } else {
+
+                            resolve(items); //finished
+                        }
+                    }
+                });
+            })
+
+            request.on("error", (err) => {
+                reject(`Failed to make get request: ${err}`);
+            });
+
+            request.end();
+
         }
 
-        //make get request
-        let request = https.request(options, (res) => {
-            let data = "";
-            let body;
+        //fetch first part
+        fetchPart(0, 0);
 
-            //listen for recieved data
-            res.on("data", (chunk) => {
-                data += chunk.toString();
-            });
-
-            //listen for completed request event
-            res.on("end", () => {
-
-                //try to convert recieved string into object
-                try {
-                    body = JSON.parse(data);
-                } catch (err) {
-                    callback(`Failed to parse response: ${err}`, null);
-                }
-
-                //Check if spotify responded with an error, otherwise callback data
-                if (body["error"]) {
-                    callback(`Spotify responded with error ${body.error.status}: ${body.error.message}`, null);
-                } else {
-
-                    //push all objects of new array to existing array
-                    body.items.every(e => itemsObj.push(e))
-
-                    //console.log(itemsObj)
-
-                    //Check if we have to fetch more parts and call function again
-                    if (body.total > (part + 1) * 100) {
-                        logger("info", `Finished fetching playlist part ${part}, waiting ${config.fetchDelay}ms before continuing with next part...`, false, true, logger.animation("loading"));
-
-                        //call this function again after fetchDelay ms with increased values
-                        setTimeout(() => fetchPart(offset + 100, part + 1), config.fetchDelay);
-                    } else {
-                        callback(null, itemsObj); //finished
-                    }
-                }
-            });
-        })
-
-        request.on("error", (err) => {
-            callback(`Failed to make get request: ${err}`, null);
-        });
-
-        request.end();
-
-    }
-
-    //fetch first part
-    fetchPart(0, 0);
+    })
 }
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@
  * Created Date: 22.04.2022 19:46:18
  * Author: 3urobeat
  * 
- * Last Modified: 14.06.2023 22:59:24
+ * Last Modified: 14.06.2023 23:23:55
  * Modified By: 3urobeat
  * 
  * Copyright (c) 2022 3urobeat <https://github.com/HerrEurobeat>
@@ -21,12 +21,14 @@ const logger = require("output-logger");
 const config = require("../config.json");
 
 const version = "1.0";
+const { fetchPlaylist } = require("./helpers/fetchPlaylist.js");
+
 
 
 /**
  * Entry point
  */
-module.exports.run = () => {
+module.exports.run = async () => {
 
     //Configure my logging library (https://github.com/HerrEurobeat/output-logger#options-1)
     logger.options({
@@ -53,120 +55,117 @@ module.exports.run = () => {
     //get playlist data from Spotify
     logger("info", "Fetching playlist data from Spotify...", false, false, logger.animation("loading"));
 
-    require("./helpers/fetchPlaylist.js").fetchPlaylist(config.playlistID, config.oAuthToken, (err, data) => {
-        
-        //check for err in callback and abort
-        if (err) {
-            logger("error", `An error ocurred while trying to fetch playlist data from Spotify!\nError: ${err}\n\nAborting...`);
-            process.exit(1);
-        }
+    let data = await fetchPlaylist(config.playlistID, config.oAuthToken)
+        .catch((err) => {
+            logger("error", `An error ocurred while trying to fetch playlist data from Spotify!\n                             Error: ${err}\n\n                             Aborting...`);
+            return process.exit(1);
+        });
 
-        logger("info", `Successfully fetched ${data.length} songs from Spotify!`);
 
-        
-        //convert each track obj into one string
-        let songsStrArr = [];
+    logger("info", `Successfully fetched ${data.length} songs from Spotify!`);
 
-        data.forEach((e, i) => {
-            let temp = ""
+    
+    //convert each track obj into one string
+    let songsStrArr = [];
 
-            //add all artists
-            e.track.artists.every((f, j) => { 
-                if (j > 0) temp += ", " //add comma infront if not first iteration
-                temp += `${f.name}`
-            })
+    data.forEach((e, i) => {
+        let temp = ""
 
-            //add track title
-            temp += ` - ${e.track.name}`
+        //add all artists
+        e.track.artists.every((f, j) => { 
+            if (j > 0) temp += ", " //add comma infront if not first iteration
+            temp += `${f.name}`
+        })
 
-            //push temp str
-            songsStrArr.push(temp);
+        //add track title
+        temp += ` - ${e.track.name}`
 
-            //check if finished
-            if (i + 1 == data.length) {
-                //logger("", songsStrArr, true) //uncomment to log all song titles that were fetched
+        //push temp str
+        songsStrArr.push(temp);
 
-                // Display warning message if arr is large af
-                if (songsStrArr.length > 2000) logger("warn", `Comparing ${data.length} song titles will take a while. Please be patient...`)
+        //check if finished
+        if (i + 1 == data.length) {
 
-                let startTimestamp = Date.now();
-                let workersDone    = 0;
+            // Display warning message if arr is large af
+            if (songsStrArr.length > 2000) logger("warn", `Comparing ${data.length} song titles will take a while. Please be patient...`)
 
-                logger("info", `Starting to compare ${songsStrArr.length} song titles...`, false, false, logger.animation("loading"));
+            let startTimestamp = Date.now();
+            let workersDone    = 0;
 
-                // Create two arrays for collecting duplicates and non-duplicates with their similarity
-                let duplicates   = [];
-                let similarities = [];
+            logger("info", `Starting to compare ${songsStrArr.length} song titles...`, false, false, logger.animation("loading"));
 
-                // Spawn worker for every 50th element in the array to multithread this thing
-                for (let i = 0; i < (songsStrArr.length / 50); i++) {
-                    const worker = new worker_threads.Worker("./src/helpers/compareStrings.js", {
-                        workerData: {
-                            songsStrArr,
-                            config,
-                            rangeStart: 50 * i,
-                            rangeEnd: (50 * i) + 50
-                        }
-                    });
+            // Create two arrays for collecting duplicates and non-duplicates with their similarity
+            let duplicates   = [];
+            let similarities = [];
 
-                    // Get result from worker
-                    worker.once("message", (data) => {
-
-                        // Filter and add unique results
-                        data.dup.forEach((e) => {
-                            if (!duplicates.includes(e)) duplicates.push(e);
-                        });
-
-                        data.sim.forEach((e, i) => {
-                            if (!similarities.some(f => e.compStr == f.compStr || e.compStr == f.compStrReversed)) similarities.push(e);
-
-                            if (data.sim.length == i + 1) workersDone++; // Mark this worker as done when all similarities have been processed
-                        });
-
-                    });
-
-                    // Start interval on last iteration that checks for done
-                    if (i + 1 >= (songsStrArr.length / 50)) {
-                        let doneInterval = setInterval(() => {
-                            if (workersDone != i + 1) return;
-                            clearInterval(doneInterval);
-
-                            similarities.sort((a, b) => { return b.similarityPerc - a.similarityPerc; }) //sort similarities descending by similarity in percent
-
-                            logger("info", "Finished comparing and sorting all song titles!", false, true);
-
-                            // Print results
-                            if (duplicates.length > 0) {
-                                let temp = "";
-                                duplicates.forEach((e) => temp += `+ ${e}\n`)
-
-                                logger("", "", true);
-                                logger("info", `I found ${duplicates.length} duplicate song titles:`)
-                                logger("", temp, true);
-                            } else {
-                                logger("info", "No duplicate song titles found!\n")
-                            }
-
-                            
-                            if (similarities.length > 0) {
-                                let temp = "";
-                                similarities.forEach((e) => temp += `+ ${e.compStr}\n`)
-
-                                logger("", "", true);
-                                logger("info", `I found ${similarities.length} similar song titles:`)
-                                logger("", temp, true);
-                            } else {
-                                logger("info", "No similar song titles found!\n")
-                            }
-
-                            
-                            let itTook = (Date.now() - startTimestamp) / 1000;
-
-                            logger("info", `Done after ${itTook} seconds!\n                             Check 'output.txt' for the full log. Exiting...\n`);
-                        }, 500);
+            // Spawn worker for every 50th element in the array to multithread this thing
+            for (let i = 0; i < (songsStrArr.length / 50); i++) {
+                const worker = new worker_threads.Worker("./src/helpers/compareStrings.js", {
+                    workerData: {
+                        songsStrArr,
+                        config,
+                        rangeStart: 50 * i,
+                        rangeEnd: (50 * i) + 50
                     }
+                });
+
+                // Get result from worker
+                worker.once("message", (data) => {
+
+                    // Filter and add unique results
+                    data.dup.forEach((e) => {
+                        if (!duplicates.includes(e)) duplicates.push(e);
+                    });
+
+                    data.sim.forEach((e, i) => {
+                        if (!similarities.some(f => e.compStr == f.compStr || e.compStr == f.compStrReversed)) similarities.push(e);
+
+                        if (data.sim.length == i + 1) workersDone++; // Mark this worker as done when all similarities have been processed
+                    });
+
+                });
+
+                // Start interval on last iteration that checks for done
+                if (i + 1 >= (songsStrArr.length / 50)) {
+                    let doneInterval = setInterval(() => {
+                        if (workersDone != i + 1) return;
+                        clearInterval(doneInterval);
+
+                        similarities.sort((a, b) => { return b.similarityPerc - a.similarityPerc; }) //sort similarities descending by similarity in percent
+
+                        logger("info", "Finished comparing and sorting all song titles!", false, true);
+
+                        // Print results
+                        if (duplicates.length > 0) {
+                            let temp = "";
+                            duplicates.forEach((e) => temp += `+ ${e}\n`)
+
+                            logger("", "", true);
+                            logger("info", `I found ${duplicates.length} duplicate song titles:`)
+                            logger("", temp, true);
+                        } else {
+                            logger("info", "No duplicate song titles found!\n")
+                        }
+
+                        
+                        if (similarities.length > 0) {
+                            let temp = "";
+                            similarities.forEach((e) => temp += `+ ${e.compStr}\n`)
+
+                            logger("", "", true);
+                            logger("info", `I found ${similarities.length} similar song titles:`)
+                            logger("", temp, true);
+                        } else {
+                            logger("info", "No similar song titles found!\n")
+                        }
+
+                        
+                        let itTook = (Date.now() - startTimestamp) / 1000;
+
+                        logger("info", `Done after ${itTook} seconds!\n                             Check 'output.txt' for the full log. Exiting...\n`);
+                    }, 500);
                 }
             }
-        })
+        }
     })
 }
